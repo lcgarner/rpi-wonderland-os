44ca9c821740a8e339944f18152579ce184e4122
Lily Garner <113074918+lcgarner@users.noreply.github.com>
2025-05-05 20:31:14 -0400

    Update io.h

--- include/io.h
+++ include/io.h
@@ -8,7 +8,6 @@ void uart_init(void);
 void uart_putc(unsigned char c);
 unsigned char uart_getc(void);
 void uart_puts(const char *str);
-void uart_puts_slow(const char *str);
 void uart_gets(char *buffer, int max_len);
 
 // utility
@@ -16,6 +15,7 @@ void clear_screen(void);
 int strcmp(const char *s1, const char *s2);
 char* int_to_string(int num);
 char** read_args(char *buf);
+void uart_puts_slow(const char *str);
 
 // mmio helpers
 void mmio_write(uint32_t reg, uint32_t data);
